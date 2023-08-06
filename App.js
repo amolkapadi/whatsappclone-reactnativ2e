3e473e0ed324b3dbcde6497a0ec4e3cb5c8b75fb
amolkapadi <amolakapadi@gmail.com>
2023-08-06 08:46:26 +0530

    updated vedio 2 code

--- App.js
+++ App.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import TopBar from './src/components/TopBar'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { NavigationContainer } from '@react-navigation/native'
 
-import ChatsScreen from './src/comp/CallsScreen';
-import StatusScreen from './src/comp/StatusScreen';
-import CallsScreen from './src/comp/CallsScreen';
-import TopBar from './src/TopBar';
+
+import ChatScreen from './src/screens/ChatScreen'
+import CallsScreen from './src/screens/CallsScreen'
+import StatusCsreen from './src/screens/StatusCsreen'
 
 const Tab = createMaterialTopTabNavigator();
 
-const App = () => {
+export default function App() {
   return (
-
     <>
-    <NavigationContainer>
+     <NavigationContainer>
       <TopBar />
-
-      
-      <Tab.Navigator
+      <Tab.Navigator  
         screenOptions={{
-          tabBarLabelStyle: { color: 'white' },
+          tabBarLabelStyle:{ color : 'white' },
           tabBarIndicatorStyle: { backgroundColor: 'white' },
-          tabBarStyle: { backgroundColor: '#128B7E' },
+          tabBarStyle: { backgroundColor: '#128B7E' }
         }}
+      
       >
-        <Tab.Screen name="Chats" component={ChatsScreen} />
-        <Tab.Screen name="Status" component={StatusScreen} />
-        <Tab.Screen name="Calls" component={CallsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-    </>
-  );
-};
+      <Tab.Screen name="Chats" component={ChatScreen} />
+      <Tab.Screen name="Status" component={StatusCsreen} />
+      <Tab.Screen name="Calls" component={CallsScreen} />
 
-export default App;
+      </Tab.Navigator>
+     </NavigationContainer>
+    </>
+  )
+}
--- src/screens/StatusCsreen.js
+++ src/screens/StatusCsreen.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function StatusScreen() {
+export default function StatusCsreen() {
   return (
     <View>
-      <Text>StatusScreen</Text>
+      <Text>StatusCsreen</Text>
     </View>
   )
 }
