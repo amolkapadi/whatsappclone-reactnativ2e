70b317a434c6831b80dfc1cca7b298ffe98fb1d5
amolkapadi <amolakapadi@gmail.com>
2023-08-06 14:12:29 +0530

    Update status screen

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,62 +1,61 @@
-import React from 'react';
-import { View, FlatList, Text, Image, StyleSheet } from 'react-native';
-import Data from '../Data';
-const UserCard = ({ name, status, profileImage }) => {
-  return (
-    <View style={styles.container}>
-      <Image source={{ uri: profileImage }} style={styles.profileImage} />
-      <View style={styles.details}>
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.status}>{status}</Text>
-      </View>
-    </View>
-  );
-};
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native'
+import React from 'react'
+import Data from '../Data'
 
-const ChatsScreen = () => {
- 
+
+const UserCard= ({name, status, profileImage})=>{
+  return(
+    <View style={styles.container}>
+        <Image source={{uri:profileImage}} style={styles.profileImage}   />
+        <View style={styles.details}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.status}>{status}</Text>
+        </View>
+    </View>
+  )
+}
+
+export default function ChatScreen() {
   return (
     <View>
-      <FlatList
+    <FlatList 
         data={Data}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <UserCard
-            name={item.name}
-            status={item.status}
-            profileImage={item.profileImage}
+        keyExtractor={(item)=> item.id}
+        renderItem={({item})=>(
+          <UserCard 
+              name={item.name}
+              status={item.status}
+              profileImage={item.profileImage}
           />
         )}
-      />
+    />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#e0e0e0',
-  },
-  profileImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    marginRight: 16,
-  },
-  details: {
-    flex: 1,
-  },
-  name: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  status: {
-    fontSize: 14,
-    color: '#666',
-  },
-});
-
-export default ChatsScreen;
+    container:{
+      flexDirection:'row',
+      alignItems:'center',
+      padding:16,
+      borderBottomWidth:1,
+      borderBottomColor:'#e0e0e0'
+    },
+    profileImage:{
+      width:50,
+      height:50,
+      borderRadius:25,
+      marginRight:16
+    },
+    details:{
+      flex:1
+    },
+    name:{
+      fontSize:16,
+      fontWeight:'bold'
+    },
+    status:{
+      fontSize:14,
+      color:'#666'
+    }
+})
--- src/screens/StatusCsreen.js
+++ src/screens/StatusCsreen.js
@@ -1,10 +1,77 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
+import Data from '../Data'
+
+
+const UserCard= ({name, status, profileImage})=>{
+  return(
+    <View style={styles.container}>
+        <Image source={{uri:profileImage}} style={styles.profileImage}   />
+        <View style={styles.details}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.status}>{status}</Text>
+        </View>
+    </View>
+  )
+}
 
 export default function StatusCsreen() {
   return (
     <View>
-      <Text>StatusCsreen</Text>
+     <View style={styles.container2}>
+        <Image style={styles.profileImage}   />
+        <View style={styles.details}>
+          <Text style={styles.name}>My Status</Text>
+          <Text style={styles.status}>1 hours ago</Text>
+        </View>
+    </View>
+    <FlatList 
+        data={Data}
+        keyExtractor={(item)=> item.id}
+        renderItem={({item})=>(
+          <UserCard 
+              name={item.name}
+              status={item.status}
+              profileImage={item.profileImage}
+          />
+        )}
+    />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{
+      flexDirection:'row',
+      alignItems:'center',
+      padding:16,
+      borderBottomColor:'#e0e0e0'
+    },
+    container2:{
+      flexDirection:'row',
+      alignItems:'center',
+      padding:16,
+      borderBottomColor:'#e0e0e0',
+      borderBottomWidth:1,
+      borderBottomColor:'#e0e0e0'
+    },
+    profileImage:{
+      width:50,
+      height:50,
+      borderRadius:25,
+      marginRight:16,
+      borderWidth: 3,  // Add border width
+      borderColor: 'green',  // Add border color
+    },
+    details:{
+      flex:1
+    },
+    name:{
+      fontSize:16,
+      fontWeight:'bold'
+    },
+    status:{
+      fontSize:14,
+      color:'#666'
+    }
+})
